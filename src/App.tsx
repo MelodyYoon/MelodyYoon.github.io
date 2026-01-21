0f27e077d0b078540286166a76d9d0ff291a7e66
MelodyYoon <melodyyoon3@gmail.com>
2026-01-20 18:51:44 -0800

    Created Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Research from './Pages/Research';
+import Projects from './Pages/Projects';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+/*
+Link: creates navigation link that update the <URL>
+Routes: container for all route definitions
+Route: defines mapping between URL path and component
+*/
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/research' element={<Research />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
