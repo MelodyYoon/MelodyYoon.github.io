b43894a212f82f58f7a75ba3cfc94120ecada03e
MelodyYoon <melodyyoon3@gmail.com>
2026-01-22 23:32:42 -0800

    Created Home page with bio description and contacts

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,22 @@
+import './Home.css';
+import headshot from '../assets/headshot.png';
+
 function Home() {
     return (
-        <div> Placeholder </div>
+        <>
+            <img className="headshot" src={headshot} alt="Headshot" />
+            <div className="homeIntro">
+                Hi ! I'm Melody Yoon{", "} a 2nd year Computer Science student at UCLA. I love creating products and continuously developing with my users' feedback. Outside of coding, I enjoy being a cellist and barista. 
+                <br />
+                <br />
+                Feel free to reach out! Always open to chat {":)"}
+            </div>
+            <div className="contacts">
+                <button className="linkedinButton" onClick={() => window.open('https://www.linkedin.com/in/melody-yoon-b5a128310', '_blank')}>Linkedin</button>
+                <div className="email">✉ meyoon@g.ucla.edu</div>
+            </div>
+
+            
+        </>
     )
 } export default Home;
